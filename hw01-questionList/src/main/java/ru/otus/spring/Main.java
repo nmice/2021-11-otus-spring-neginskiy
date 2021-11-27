a924ff13e110945f21a922aea6693c1daaccbfc2
N.Mice <n.mice@mail.ru>
2021-11-27 19:47:16 +0300

    added questions.csv

--- hw01-questionList/src/main/java/ru/otus/spring/Main.java
+++ hw01-questionList/src/main/java/ru/otus/spring/Main.java
@@ -1,8 +1,11 @@
 package ru.otus.spring;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        String fileName = "questions.csv";
     }
 }
