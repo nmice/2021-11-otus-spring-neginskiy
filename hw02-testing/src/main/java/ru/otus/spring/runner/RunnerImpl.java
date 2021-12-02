191ea40ca54023560cc02e13b77ff8949c62f2c3
N.Mice <n.mice@mail.ru>
2021-12-03 02:44:44 +0300

    hw02 - added logic

--- hw02-testing/src/main/java/ru/otus/spring/runner/RunnerImpl.java
+++ hw02-testing/src/main/java/ru/otus/spring/runner/RunnerImpl.java
@@ -6,7 +6,10 @@ import ru.otus.spring.service.InputOutputService;
 import ru.otus.spring.service.InputOutputServiceImpl;
 import ru.otus.spring.service.QuestionService;
 
+import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class RunnerImpl implements Runner {
@@ -19,11 +22,44 @@ public class RunnerImpl implements Runner {
 
     @Override
     public void run() {
-        List<Question> questions = service.getAllQuestions();
-        InputOutputService inOutService = new InputOutputServiceImpl(System.in, System.out);
-        inOutService.output("QUESTIONS:");
-        questions.forEach(q -> inOutService.output(q.getQuestion()));
-        System.out.println("\r\nANSWERS:");
-        questions.forEach(q -> inOutService.output(q.getAnswer()));
+        try {
+            List<Question> questions = service.getAllQuestions();
+            InputOutputService inOutService = new InputOutputServiceImpl(System.in, System.out);
+
+            inOutService.output("Input your firstname:");
+            String firstname = inOutService.input();
+            inOutService.output("Input your secondname:");
+            String secondname = inOutService.input();
+            inOutService.output("LETS GO TEST");
+
+            int score = 0;
+            // Создаем упорядоченную по id коллекцию ответов
+            List<String> answersWithId = questions.stream()
+                    .map(q -> (q.getId() + ". " + q.getAnswer()))
+                    .collect(Collectors.toList());
+            // Перемешиваем вопросы
+            Collections.shuffle(questions);
+            // Проходим в случайном порядке все вопросы
+            for (Question question : questions) {
+                // Печатаем вопрос
+                inOutService.output(question.getQuestion() + ". What movie is this scene from?");
+                // Печатаем варианты ответов
+                answersWithId.forEach(inOutService::output);
+                // Запросить ввод ответа (id)
+                int answerId = Integer.parseInt(inOutService.input());
+                // Сверить ответ и вопрос по id
+                if (answerId == question.getId()) {
+                    // При совпадении защитать +балл
+                    score++;
+                }
+            }
+            // Вывести результат ФИ + количество верных ответов
+            inOutService.output("YOUR RESULT:");
+            inOutService.output(firstname + " " + secondname + ", You answered " +
+                    (score == questions.size() ? "all the" : score) +
+                    " questions correctly.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
