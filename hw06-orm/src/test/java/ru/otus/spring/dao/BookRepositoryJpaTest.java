69ab073cf6818335df56f55322608c88ee45352e
N.Mice <n.mice@mail.ru>
2022-04-23 02:47:01 +0300

    hw06 - past

--- hw06-orm/src/test/java/ru/otus/spring/dao/BookRepositoryJpaTest.java
+++ hw06-orm/src/test/java/ru/otus/spring/dao/BookRepositoryJpaTest.java
@@ -1,5 +1,6 @@
 package ru.otus.spring.dao;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,16 +13,17 @@ import ru.otus.spring.domain.Genre;
 import ru.otus.spring.repository.BookRepositoryJpa;
 import ru.otus.spring.repository.CommentaryRepositoryJpa;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 
-@DisplayName("Dao для работы с книгами")
+@DisplayName("Repository для работы с книгами должен ")
 @DataJpaTest
 @Import({BookRepositoryJpa.class, CommentaryRepositoryJpa.class})
-class BookDaoImplTest {
+class BookRepositoryJpaTest {
 
     private static final int EXPECTED_BOOK_COUNT = 3;
     private static final int EXPECTED_NUMBER_OF_BOOKS = 3;
@@ -38,9 +40,54 @@ class BookDaoImplTest {
     @Autowired
     private TestEntityManager em;
 
-    @DisplayName("должен корректно сохранять книгу в бд")
+    @DisplayName("корректно добавлять книгу в БД")
     @Test
-    void shouldSaveBook() {
+    void shouldInsertBook() {
+        Book book = new Book(4L, "New book title", new Author(1L, "Miguel De Cervantes"), new Genre(2L, "Programming"));
+        bookRepository.save(book);
+        Book actualBook = bookRepository.findById(4L).get();
+        Assertions.assertThat(actualBook).isEqualTo(book);
+    }
+
+    @DisplayName("верно считать количество книг")
+    @Test
+    void shouldReturnCorrectBookCount() {
+        Assertions.assertThat(bookRepository.getCount()).isEqualTo(3);
+    }
+
+    @DisplayName("получать все книги")
+    @Test
+    void shouldReturnCorrectBookList() {
+        Book book1 = new Book(1L, "Don Quixote", new Author(1L, "Miguel De Cervantes"), new Genre(1L, "Novel"));
+        Book book2 = new Book(2L, "Java Headfirst", new Author(2L, "Kathy Sierra"), new Genre(2L, "Programming"));
+        Book book3 = new Book(3L, "After three it`s too late", new Author(3L, "Masaru Ibuka"), new Genre(3L, "Psychology"));
+        List<Book> books = new ArrayList<>();
+        books.add(book1);
+        books.add(book2);
+        books.add(book3);
+        List<Book> actualBooks = bookRepository.findAll();
+        Assertions.assertThat(actualBooks).isEqualTo(books);
+    }
+
+    @DisplayName("получать нужную книгу по id")
+    @Test
+    void shouldReturnCorrectBookById() {
+        Book book = new Book(1L, "Don Quixote", new Author(1L, "Miguel De Cervantes"), new Genre(1L, "Novel"));
+        Book actualBook = bookRepository.findById(1L).get();
+        Assertions.assertThat(actualBook).isEqualTo(book);
+    }
+
+    @DisplayName("удалять нужную книгу из БД")
+    @Test
+    void shouldCorrectDeleteBookById() {
+        Assertions.assertThat(bookRepository.getCount()).isEqualTo(3);
+        bookRepository.deleteById(1L);
+        Assertions.assertThat(bookRepository.getCount()).isEqualTo(2);
+    }
+
+/*    @DisplayName("корректно добавлять книгу в бд")
+    @Test
+    void shouldInsertBook() {
         Author author = new Author(1L, "Ray Bradbury");
         Genre genre = new Genre(1L, "451 Fahrenheit");
         var book = new Book(NEW_BOOK_TITLE, author, genre);
@@ -54,7 +101,7 @@ class BookDaoImplTest {
                 .matches(b -> b.getGenre() != null);
     }
 
-    @DisplayName("должен загружать информацию о нужной книге по её Id")
+    @DisplayName("корректно загружать информацию о нужной книге по её Id")
     @Test
     void shouldFindBookById() {
         Optional<Book> optionalActualBook = bookRepository.findById(FIRST_BOOK_ID);
@@ -62,7 +109,7 @@ class BookDaoImplTest {
         assertThat(optionalActualBook).isPresent().get().isEqualTo(expectedBook);
     }
 
-    @DisplayName("должен загружать список всех книг с информацией об авторе и жанре")
+    @DisplayName("корректно загружать список всех книг с информацией об авторе и жанре")
     @Test
     void shouldReturnCorrectBookListWithGenreAndAuthor() {
         List<Book> books = bookRepository.findAll();
@@ -72,7 +119,7 @@ class BookDaoImplTest {
                 .allMatch(book -> book.getAuthor() != null);
     }
 
-    @DisplayName(" должен загружать информацию о нужной книге по ее имени")
+    @DisplayName("корректно загружать информацию о нужной книге по ее имени")
     @Test
     void shouldFindBookByName() {
         Book firstBook = em.find(Book.class, FIRST_BOOK_ID);
@@ -80,7 +127,7 @@ class BookDaoImplTest {
         assertThat(books).containsOnlyOnce(firstBook);
     }
 
-    @DisplayName(" должен изменять наименование нужной книге по ее Id")
+    @DisplayName("корректно изменять наименование нужной книге по ее Id")
     @Test
     void shouldUpdateBookNameById() {
         Book firstBook = em.find(Book.class, FIRST_BOOK_ID);
@@ -93,7 +140,7 @@ class BookDaoImplTest {
         assertThat(updatedBook.getTitle()).isNotEqualTo(oldName).isEqualTo(NEW_BOOK_TITLE);
     }
 
-    @DisplayName(" должен удалять нужную книгу по ее Id")
+    @DisplayName("корректно удалять нужную книгу по ее Id")
     @Test
     void shouldDeleteBookNameById() {
         em.clear();
@@ -103,10 +150,9 @@ class BookDaoImplTest {
         assertThat(deletedBook).isNull();
     }
 
-    @DisplayName(" должен выводить правильное количество книг")
+    @DisplayName("верно считать количество книг")
     @Test
     void shouldReturnCorrectBookCount() {
-        long count = bookRepository.getCount();
-        assertThat(count).isEqualTo(EXPECTED_BOOK_COUNT);
-    }
+        Assertions.assertThat(bookRepository.getCount()).isEqualTo(3);
+    }*/
 }
